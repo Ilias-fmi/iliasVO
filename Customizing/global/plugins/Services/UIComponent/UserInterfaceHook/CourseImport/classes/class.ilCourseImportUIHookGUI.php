de4a4bb1656b362e50f731e42507fccda47d675f
merglml <merglml@studi.informatik.uni-stuttgart.de>
2016-12-23 13:00:33 +0100

    Plugin zur Kurstabs verschoben

--- Customizing/global/plugins/Services/UIComponent/UserInterfaceHook/CourseImport/classes/class.ilCourseImportUIHookGUI.php
+++ Customizing/global/plugins/Services/UIComponent/UserInterfaceHook/CourseImport/classes/class.ilCourseImportUIHookGUI.php
@@ -32,25 +32,20 @@ class ilCourseImportUIHookGUI extends ilUIHookPluginGUI
 
 	function modifyGUI($a_comp, $a_part, $a_par = array())
 	{
-		if ($a_part == 'tabs' && ilObject::_lookupType($_GET['ref_id'], true) == 'crss') {
+		if ($_GET["baseClass"] == 'ilRepositoryGUI'&& $a_part == 'tabs' ){
 			/** @var ilTabsGUI $tabs */
 			$tabs = $a_par['tabs'];
 
-            $this->ctrl->setParameterByClass('ilcourseimportgui', 'ref_id', $_GET['ref_id']);
-            $link = $this->ctrl->getLinkTargetByClass(array('ilUIPluginRouterGUI', 'ilCourseImportGUI'));
-
             $this->ctrl->setParameterByClass('ilcourseimportgroupgui', 'ref_id', $_GET['ref_id']);
             $link1 = $this->ctrl->getLinkTargetByClass(array('ilUIPluginRouterGUI', 'ilCourseImportGroupGUI'));
-
-
-            $tabs->addTab('course_import', $this->pl->txt('tab_course_import'), $link);
-
             $tabs->addTab('course_management', $this->pl->txt('tab_course_management'), $link1);
 
-            //$this->ctrl->setParameterByClass('ilcourseimportgroupdisplaygui', 'ref_id', $_GET['ref_id']);
-            //$link2 = $this->ctrl->getLinkTargetByClass(array('ilUIPluginRouterGUI', 'ilCourseImportGroupDisplayGUI'));
-            //$tabs->addTab('course_management_tbl', $this->pl->txt('tab_course_management_tbl'), $link2);
-
+        }
+        if ($a_part == 'tabs'&& ilObject::_lookupType($_GET['ref_id'], true) == 'crss'){
+            $tabs1 = $a_par['tabs'];
+            $this->ctrl->setParameterByClass('ilcourseimportgui', 'ref_id', $_GET['ref_id']);
+            $link = $this->ctrl->getLinkTargetByClass(array('ilUIPluginRouterGUI', 'ilCourseImportGUI'));
+            $tabs1->addTab('course_import', $this->pl->txt('tab_course_import'), $link);
         }
 
 	}
